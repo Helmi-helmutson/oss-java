99b449a1a98c5e37ec977f6147835698467a9e0b
Peter Grill <pg@extis.de>
2017-01-31 12:42:19 +0100

    added sample for userid creation and password creation

---
+++ oss-dao/src/test/java/de/openschoolserver/TestUtil.java
@@ -0,0 +1,15 @@
+package de.openschoolserver;
+
+import java.util.Date;
+
+import de.extis.core.util.UserUtil;
+
+public class TestUtil {
+	public static void main (String[] args) {
+		System.out.println("Random Password 8 digits: " + UserUtil.createRandomPassword(8));
+		System.out.println("Random Password 8 digits with given chars: " + UserUtil.createRandomPassword(8,"ABCabc897.!"));
+	
+		String userid = UserUtil.createUserId("Péter","Varkoly",new Date(), true, true, "G4N2Y2");
+		System.out.println("CreateUserId:");
+	}
+}
