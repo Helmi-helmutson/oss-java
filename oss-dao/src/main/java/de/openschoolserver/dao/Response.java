c4cf6796c83ffe16e53020e1a4446ac35b3ce867
Peter Varkoly <varkoly@suse.com>
2017-05-16 22:18:09 +0200

    Join the AccessInRoomTables

--- oss-dao/src/main/java/de/openschoolserver/dao/Response.java
+++ oss-dao/src/main/java/de/openschoolserver/dao/Response.java
@@ -1,56 +1,75 @@
 package de.openschoolserver.dao;
-import java.io.Serializable;
-import java.util.Date;
-import java.util.List;
 
+import java.io.Serializable;
 import javax.persistence.*;
 
+
+/**
+ * The persistent class for the Responses database table.
+ * 
+ */
 @Entity
 @Table(name="Responses")
-@NamedQueries({
-	@NamedQuery(name="Response.findAll", query="SELECT r FROM Response r"),
-})
-@SequenceGenerator(name="seq", initialValue=1, allocationSize=100)
+@NamedQuery(name="Response.findAll", query="SELECT r FROM Response r")
 public class Response implements Serializable {
+	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="seq")
-	long id;
+	@SequenceGenerator(name="RESPONSES_ID_GENERATOR", sequenceName="SEQ")
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="RESPONSES_ID_GENERATOR")
+	private long id;
 
 	private String code;
-	private String text;
-	
+	private String value;
 	@ManyToOne
-	Session session;
+    Session session;
 
-        @Override
-        public boolean equals(Object obj) {
-                if (obj instanceof Response && obj !=null) {
-                        return getId() == ((Response)obj).getId();
-                }
-                return super.equals(obj);
-        }
+	@Column(name="session_id")
+	private java.math.BigInteger sessionId;
 	
-	public Response(Session session,String code, String text){
-		this.session = session;
-		this.code = code;
-		this.text = text;
-	}
-	
-	public void setId(long id) {
-		this.id = id;
-	}
-	
+    @Override
+    public boolean equals(Object obj) {
+            if (obj instanceof Response && obj !=null) {
+                    return getId() == ((Response)obj).getId();
+            }
+            return super.equals(obj);
+    }
+
+    public Response() {
+    }
+
+    public Response(Session session,String code, String value){
+            this.session = session;
+            this.code    = code;
+            this.value   = value;
+    }
+   
 	public long getId() {
 		return this.id;
 	}
-	
-	public String getText() {
-		return this.text;
+
+	public void setId(long id) {
+		this.id = id;
 	}
-	
+
 	public String getCode() {
 		return this.code;
+	}
+
+	public java.math.BigInteger getSessionId() {
+		return this.sessionId;
+	}
+
+	public void setSessionId(java.math.BigInteger sessionId) {
+		this.sessionId = sessionId;
+	}
+
+	public String getValue() {
+		return this.value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
 	}
 
 }
