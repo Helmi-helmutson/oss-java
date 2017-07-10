842005df97524e0778a9ec2acac386baf3f5e836
Peter Grill <pg@extis.de>
2017-07-10 09:04:21 +0200

    added missing imports

--- oss-api/src/main/java/de/openschoolserver/api/resourceimpl/ImporterResourceImpl.java
+++ oss-api/src/main/java/de/openschoolserver/api/resourceimpl/ImporterResourceImpl.java
@@ -7,6 +7,8 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Random;
