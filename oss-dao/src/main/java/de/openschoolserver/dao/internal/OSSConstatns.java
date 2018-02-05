fdb6228300d4de0b2842dbd8b71117a31274a827
Peter Varkoly <varkoly@suse.com>
2018-02-05 10:59:46 +0100

    add missed file

---
+++ oss-dao/src/main/java/de/openschoolserver/dao/internal/OSSConstatns.java
@@ -0,0 +1,11 @@
+package de.openschoolserver.dao.internal;
+
+public interface OSSConstatns {
+	
+	String roleTeacher  = "teachers";
+	String roleStudent  = "studetns";
+	String roleSysadmin = "sysadmins";
+	String roleAdministratrion = "administration";
+	String roleWorkstation = "workstations";
+
+}
