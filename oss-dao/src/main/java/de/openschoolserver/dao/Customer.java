a5c809470a86318945cdd52fb52af5705c9853c3
Peter Varkoly <varkoly@suse.com>
2018-02-13 08:10:09 +0100

    Now we can save customers and institutes and regcodes.

---
+++ oss-dao/src/main/java/de/openschoolserver/dao/Customer.java
@@ -0,0 +1,177 @@
+package de.openschoolserver.dao;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+
+/**
+ * The persistent class for the Customers database table.
+ * 
+ */
+@Entity
+@Table(name="Customers")
+@NamedQuery(name="Customer.findAll", query="SELECT c FROM Customer c")
+public class Customer implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private Long id;
+
+	private String address1;
+
+	private String address2;
+
+	private String category;
+
+	private String city;
+
+	private String contact;
+
+	private String country;
+
+	@Lob
+	private byte[] description;
+
+	private String name;
+
+	private String name2;
+
+	private String postalCode;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date recDate;
+
+	private String state;
+
+	private String telephone;
+
+	//bi-directional many-to-one association to Cephalix Institutes
+	@OneToMany(mappedBy="cephalixinstitute")
+	private List<CephalixInstitute> cephalixInstitutes;
+
+	public Customer() {
+	}
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAddress1() {
+		return this.address1;
+	}
+
+	public void setAddress1(String address1) {
+		this.address1 = address1;
+	}
+
+	public String getAddress2() {
+		return this.address2;
+	}
+
+	public void setAddress2(String address2) {
+		this.address2 = address2;
+	}
+
+	public String getCategory() {
+		return this.category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getCity() {
+		return this.city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getContact() {
+		return this.contact;
+	}
+
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+
+	public String getCountry() {
+		return this.country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public byte[] getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(byte[] description) {
+		this.description = description;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName2() {
+		return this.name2;
+	}
+
+	public void setName2(String name2) {
+		this.name2 = name2;
+	}
+
+	public String getPostalCode() {
+		return this.postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public Date getRecDate() {
+		return this.recDate;
+	}
+
+	public void setRecDate(Date recDate) {
+		this.recDate = recDate;
+	}
+
+	public String getState() {
+		return this.state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getTelephone() {
+		return this.telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public List<CephalixInstitute> getCephalixInstitutes() {
+		return this.cephalixInstitutes;
+	}
+
+	public void setCephalixInstitutes(List<CephalixInstitute> cephalixinstitutes) {
+		this.cephalixInstitutes = cephalixinstitutes;
+	}
+
+}
---
+++ oss-dao/src/main/java/de/openschoolserver/dao/Regcode.java
@@ -0,0 +1,87 @@
+package de.openschoolserver.dao;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.Date;
+
+
+/**
+ * The persistent class for the Regcode database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Regcode.findAll", query="SELECT r FROM Regcode r")
+public class Regcode implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private Long id;
+
+	private String name;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name="rec_date")
+	private Date recDate;
+
+	private String status;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date validity;
+
+        //bi-directional many-to-one association to CephalixInstitue
+        @ManyToOne
+        @JsonIgnore
+        private CephalixInstitue cephalixInstitute;
+
+	public Regcode() {
+	}
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getRecDate() {
+		return this.recDate;
+	}
+
+	public void setRecDate(Date recDate) {
+		this.recDate = recDate;
+	}
+
+	public String getStatus() {
+		return this.status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Date getValidity() {
+		return this.validity;
+	}
+
+	public void setValidity(Date validity) {
+		this.validity = validity;
+	}
+
+	public CephalixInstitute getCephalixInstitute() {
+		return this.cephalixInstitute;
+	}
+
+	public void setCephalixInstitute(CephalixInstitute cephalixInstitute) {
+		this.cephalixInstitute = cephalixInstitute;
+	}
+
+}
