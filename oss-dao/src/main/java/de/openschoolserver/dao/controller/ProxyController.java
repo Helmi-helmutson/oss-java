30a029e5096e9e23f044a5fb1754b8d6a0b98a3a
Peter Varkoly <varkoly@suse.com>
2018-03-21 08:12:28 +0100

    Fix typo

--- oss-dao/src/main/java/de/openschoolserver/dao/controller/ProxyController.java
+++ oss-dao/src/main/java/de/openschoolserver/dao/controller/ProxyController.java
@@ -5,9 +5,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
