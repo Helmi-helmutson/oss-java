cf28b259da56bdf5d5aeaede4805a86e4a598eec
Peter Varkoly <varkoly@suse.com>
2018-04-01 19:57:45 +0200

    Add missed file.

--- oss-dao/src/main/java/de/openschoolserver/dao/Translation.java
+++ oss-dao/src/main/java/de/openschoolserver/dao/Translation.java
@@ -1,68 +1,54 @@
-/* (c) 2017 Péter Varkoly <peter@varkoly.de> - all rights reserved */
 package de.openschoolserver.dao;
 
 import java.io.Serializable;
+import java.lang.Long;
+import java.lang.String;
 import javax.persistence.*;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 
 /**
- * The persistent class for the Translations database table.
- * 
+ * Entity implementation class for Entity: Translation
+ *
  */
 @Entity
 @Table(name="Translations")
 @NamedQueries({
-	@NamedQuery(name="Translation.findAll", query="SELECT t FROM Translation t"),
-	@NamedQuery(name="Translation.find",	query="SELECT t FROM Translation t WHERE t.lang = :lang AND t.string = :string")
+	@NamedQuery(name="Translation.findAll",   query="SELECT t FROM Translation t"),
+	@NamedQuery(name="Translation.findByLang",query="SELECT t FROM Translation t WHERE t.lang = :lang"),
+	@NamedQuery(name="Translation.find",	  query="SELECT t FROM Translation t WHERE t.lang = :lang AND t.string = :string")
 })
 public class Translation implements Serializable {
+
+	
+	private Long id;
+	private String lang;
+	private String string;
+	private String value;
 	private static final long serialVersionUID = 1L;
 
-	@Id
-	@SequenceGenerator(name="TRANSLATIONS_ID_GENERATOR" )
-	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="TRANSLATIONS_ID_GENERATOR")
-	private Long id;
-
-	private String lang;
-
-	private String string;
-
-	private String value;
-
 	public Translation() {
-	}
-	
-	public Translation(String lang, String string, String value) {
-		this.lang   = lang;
-		this.string = string;
-		this.value  = value;
-	}
-
+		super();
+	}   
 	public Long getId() {
 		return this.id;
 	}
 
 	public void setId(Long id) {
 		this.id = id;
-	}
-
+	}   
 	public String getLang() {
 		return this.lang;
 	}
 
 	public void setLang(String lang) {
 		this.lang = lang;
-	}
-
+	}   
 	public String getString() {
 		return this.string;
 	}
 
 	public void setString(String string) {
 		this.string = string;
-	}
-
+	}   
 	public String getValue() {
 		return this.value;
 	}
@@ -70,40 +56,5 @@ public class Translation implements Serializable {
 	public void setValue(String value) {
 		this.value = value;
 	}
-
-	@Override
-	public String toString() {
-		try {
-			return new ObjectMapper().writeValueAsString(this);
-		} catch (Exception e) {
-			return "{ \"ERROR\" : \"CAN NOT MAP THE OBJECT\" }";
-		}
-	}
-	
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Translation other = (Translation) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-
+   
 }
