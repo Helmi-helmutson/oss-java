6ad014a22ba4afbe87ecb49386dade06700a54e5
Peter Varkoly <varkoly@suse.com>
2018-04-02 08:40:21 +0200

    Implementing some important constants.

--- oss-dao/src/main/java/de/openschoolserver/dao/internal/OSSConstatns.java
+++ oss-dao/src/main/java/de/openschoolserver/dao/internal/OSSConstatns.java
@@ -1,11 +1,33 @@
 package de.openschoolserver.dao.internal;
 
+import java.nio.file.attribute.FileAttribute;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.util.Set;
+
 public interface OSSConstatns {
-	
+
 	String roleTeacher  = "teachers";
 	String roleStudent  = "studetns";
 	String roleSysadmin = "sysadmins";
 	String roleAdministratrion = "administration";
 	String roleWorkstation = "workstations";
 
+    static FileAttribute<Set<PosixFilePermission>> privatDirAttribute      = PosixFilePermissions.asFileAttribute( PosixFilePermissions.fromString("rwx------"));
+    static FileAttribute<Set<PosixFilePermission>> privatFileAttribute     = PosixFilePermissions.asFileAttribute( PosixFilePermissions.fromString("rw-------"));
+    static FileAttribute<Set<PosixFilePermission>> groupReadFileAttribute  = PosixFilePermissions.asFileAttribute( PosixFilePermissions.fromString("rw-r-----"));
+    static FileAttribute<Set<PosixFilePermission>> groupWriteFileAttribute = PosixFilePermissions.asFileAttribute( PosixFilePermissions.fromString("rw-rw----"));
+    static FileAttribute<Set<PosixFilePermission>> groupReadDirAttribute   = PosixFilePermissions.asFileAttribute( PosixFilePermissions.fromString("rwxr-x---"));
+    static FileAttribute<Set<PosixFilePermission>> groupWriteDireAttribute = PosixFilePermissions.asFileAttribute( PosixFilePermissions.fromString("rwxrwx---"));
+
+    static Set<PosixFilePermission> privatDirPermission      = PosixFilePermissions.fromString("rwx------");
+    static Set<PosixFilePermission> privatFilePermission     = PosixFilePermissions.fromString("rw-------");
+    static Set<PosixFilePermission> groupReadFilePermission  = PosixFilePermissions.fromString("rw-r-----");
+    static Set<PosixFilePermission> groupWriteFilePermission = PosixFilePermissions.fromString("rw-rw----");
+    static Set<PosixFilePermission> groupReadDirPermission   = PosixFilePermissions.fromString("rwxr-x---");
+    static Set<PosixFilePermission> groupWriteDirePermission = PosixFilePermissions.fromString("rwxrwx---");
+    static Set<PosixFilePermission> worldReadFilePermission  = PosixFilePermissions.fromString("rw-r--r--");
+    static Set<PosixFilePermission> worldWriteFilePermission = PosixFilePermissions.fromString("rw-rw-rw-");
+    static Set<PosixFilePermission> worldReadDirPermission   = PosixFilePermissions.fromString("rwxr-xr-x");
+    static Set<PosixFilePermission> worldWriteDirePermission = PosixFilePermissions.fromString("rwxrwxrwx");
 }
