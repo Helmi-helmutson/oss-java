18c0c24b836e024551664dca746b9e6b101c862c
Peter Varkoly <varkoly@suse.com>
2018-04-19 18:31:22 +0200

    Code clean up

--- oss-dao/src/main/java/de/openschoolserver/dao/internal/ImportHandler.java
+++ oss-dao/src/main/java/de/openschoolserver/dao/internal/ImportHandler.java
@@ -1,13 +1,11 @@
 package de.openschoolserver.dao.internal;
 
 import java.io.File;
-
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
-import java.math.BigInteger;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
