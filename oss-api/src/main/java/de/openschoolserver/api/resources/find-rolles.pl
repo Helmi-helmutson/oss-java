c9607b5af7e0e12b8e92f9c2272db66a7441d83a
Peter Varkoly <varkoly@suse.com>
2018-07-15 20:57:56 +0200

    adhoclan* acls are not standard.

--- oss-api/src/main/java/de/openschoolserver/api/resources/find-rolles.pl
+++ oss-api/src/main/java/de/openschoolserver/api/resources/find-rolles.pl
@@ -11,6 +11,7 @@ foreach( split /\n/, $ROLES )
 	{
 		my $r = $1;
 		next if( $r eq "printers.add" );
+		next if( $r =~ /adhoclan*/ );
 		$hroles->{$1} = 1;
 		if( $r =~ /.search/ || $r =~ /education/ ) {
 			$forTeachers->{$r} = 1;
@@ -32,4 +33,3 @@ foreach( sort keys %$forTeachers )
 	print "INSERT INTO Acls VALUES(NULL,NULL,2,'$_','Y',6);\n";
 }
 print "INSERT INTO Acls VALUES(NULL,1,NULL,'printers.add','Y',6);\n";
-
