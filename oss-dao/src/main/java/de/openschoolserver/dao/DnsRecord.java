2f348d3275fd36e8d639d8970e4b392e5f94b4d2
Peter Varkoly <varkoly@suse.com>
2018-09-06 10:36:27 +0200

    Add missed object.

---
+++ oss-dao/src/main/java/de/openschoolserver/dao/DnsRecord.java
@@ -0,0 +1,52 @@
+package de.openschoolserver.dao;
+
+public class DnsRecord {
+	
+	private String domainName;
+	private String recordType;
+	private String recordName;
+	private String recordData;
+
+	public DnsRecord() {
+	}
+
+	public DnsRecord(String domainName,String recordType, String recordName, String recordData) {
+		this.domainName = domainName;
+		this.recordType = recordType;
+		this.recordName = recordName;
+		this.recordData = recordData;
+	}
+
+	public String getDomainName() {
+		return domainName;
+	}
+
+	public void setDomainName(String domainName) {
+		this.domainName = domainName;
+	}
+
+	public String getRecordType() {
+		return recordType;
+	}
+
+	public void setRecordType(String recordType) {
+		this.recordType = recordType;
+	}
+
+	public String getRecordName() {
+		return recordName;
+	}
+
+	public void setRecordName(String recordName) {
+		this.recordName = recordName;
+	}
+
+	public String getRecordData() {
+		return recordData;
+	}
+
+	public void setRecordData(String recordData) {
+		this.recordData = recordData;
+	}
+
+}
