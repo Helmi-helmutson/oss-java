d36cdae45395fc3810ad7b7418130c5f0ec536d9
Peter Varkoly <varkoly@suse.com>
2019-01-14 11:06:18 +0100

    Fix search.

--- oss-dao/data/updates/005-ManuallyInstalled.sh
+++ oss-dao/data/updates/005-ManuallyInstalled.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export HOME="/root"
 
-echo "UPDATE  SoftwareStatus AS ss LEFT JOIN SoftwareVersions AS sv ON ss.softwareversion_id = sv.id LEFT JOIN Softwares AS s ON sv.software_id = s.id SET ss.status='MI' WHERE s.manually = 'Y';" | mysql OSS 
+echo "UPDATE  SoftwareStatus AS ss LEFT JOIN SoftwareVersions AS sv ON ss.softwareversion_id = sv.id LEFT JOIN Softwares AS s ON sv.software_id = s.id SET ss.status='MI' WHERE s.manually = 'Y' AND ss.status='I';" | mysql OSS 
 
 if [ -e /usr/lib/systemd/system/cephalix-api.service ]; then
 	systemctl restart cephalix-api
