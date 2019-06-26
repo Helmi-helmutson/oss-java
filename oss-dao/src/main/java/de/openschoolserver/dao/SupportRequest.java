7faf1fd5113d9ff138c73a2d99e08503fa38078d
Peter Varkoly <varkoly@suse.com>
2019-06-26 14:55:55 +0200

    Use tab.

--- oss-dao/src/main/java/de/openschoolserver/dao/SupportRequest.java
+++ oss-dao/src/main/java/de/openschoolserver/dao/SupportRequest.java
@@ -4,27 +4,27 @@ package de.openschoolserver.dao;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class SupportRequest {
-    public enum SUPPORT_TYPE {
-        Error, FeatureRequest, Feedback, ProductOrder, OfferInq, SalesInq
-    };
+	public enum SUPPORT_TYPE {
+		Error, FeatureRequest, Feedback, ProductOrder, OfferInq, SalesInq
+	};
 
-    private String email;
-    private String subject;
-    private String description;
-    private String regcode;
+	private String email;
+	private String subject;
+	private String description;
+	private String regcode;
 
 	private String product; // OSS, OSSClient, Cephalix
-    private String firstname;
-    private String lastname;
-    private String company;
-    private SUPPORT_TYPE supporttype; // Error, FeatureRequest, Feedback, ProductOrder, OfferInq, SalesInq
+	private String firstname;
+	private String lastname;
+	private String company;
+	private SUPPORT_TYPE supporttype; // Error, FeatureRequest, Feedback, ProductOrder, OfferInq, SalesInq
 
-    // response fields
-    private String regcodeValidUntil;
-    private String status;
-    private String requestDate;
-    private String ticketno;
-    private String ticketResponseInfo;
+	// response fields
+	private String regcodeValidUntil;
+	private String status;
+	private String requestDate;
+	private String ticketno;
+	private String ticketResponseInfo;
 
 	@Override
 	public String toString() {
@@ -35,118 +35,118 @@ public class SupportRequest {
 		}
 	}
 
-    public SupportRequest() {
+	public SupportRequest() {
 
-    }
-    public String getTicketResponseInfo() {
+	}
+	public String getTicketResponseInfo() {
 		return ticketResponseInfo;
 	}
 
 	public void setTicketResponseInfo(String ticketResponseInfo) {
 		this.ticketResponseInfo = ticketResponseInfo;
 	}
-    public String getEmail() {
-        return email;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public String getSubject() {
-        return subject;
-    }
+	public String getSubject() {
+		return subject;
+	}
 
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
 
-    public String getDescription() {
-        return description;
-    }
+	public String getDescription() {
+		return description;
+	}
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-    public String getRegcode() {
-        return regcode;
-    }
+	public String getRegcode() {
+		return regcode;
+	}
 
-    public void setRegcode(String regcode) {
-        this.regcode = regcode;
-    }
+	public void setRegcode(String regcode) {
+		this.regcode = regcode;
+	}
 
-    public String getRegcodeValidUntil() {
-        return regcodeValidUntil;
-    }
+	public String getRegcodeValidUntil() {
+		return regcodeValidUntil;
+	}
 
-    public void setRegcodeValidUntil(String regcodeValidUntil) {
-        this.regcodeValidUntil = regcodeValidUntil;
-    }
+	public void setRegcodeValidUntil(String regcodeValidUntil) {
+		this.regcodeValidUntil = regcodeValidUntil;
+	}
 
-    public String getStatus() {
-        return status;
-    }
+	public String getStatus() {
+		return status;
+	}
 
-    public void setStatus(String status) {
-        this.status = status;
-    }
+	public void setStatus(String status) {
+		this.status = status;
+	}
 
-    public String getRequestDate() {
-        return requestDate;
-    }
+	public String getRequestDate() {
+		return requestDate;
+	}
 
-    public void setRequestDate(String requestDate) {
-        this.requestDate = requestDate;
-    }
+	public void setRequestDate(String requestDate) {
+		this.requestDate = requestDate;
+	}
 
-    public String getTicketno() {
-        return ticketno;
-    }
+	public String getTicketno() {
+		return ticketno;
+	}
 
-    public void setTicketno(String ticketno) {
-        this.ticketno = ticketno;
-    }
+	public void setTicketno(String ticketno) {
+		this.ticketno = ticketno;
+	}
 
-    public String getProduct() {
-        return product;
-    }
+	public String getProduct() {
+		return product;
+	}
 
-    public void setProduct(String product) {
-        this.product = product;
-    }
+	public void setProduct(String product) {
+		this.product = product;
+	}
 
-    public String getFirstname() {
-        return firstname;
-    }
+	public String getFirstname() {
+		return firstname;
+	}
 
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
 
-    public String getLastname() {
-        return lastname;
-    }
+	public String getLastname() {
+		return lastname;
+	}
 
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
 
-    public String getCompany() {
-        return company;
-    }
+	public String getCompany() {
+		return company;
+	}
 
-    public void setCompany(String company) {
-        this.company = company;
-    }
+	public void setCompany(String company) {
+		this.company = company;
+	}
 
-    public SUPPORT_TYPE getSupporttype() {
-        return supporttype;
-    }
+	public SUPPORT_TYPE getSupporttype() {
+		return supporttype;
+	}
 
-    public void setSupporttype(SUPPORT_TYPE supporttype) {
-        this.supporttype = supporttype;
-    }
+	public void setSupporttype(SUPPORT_TYPE supporttype) {
+		this.supporttype = supporttype;
+	}
 
 }
