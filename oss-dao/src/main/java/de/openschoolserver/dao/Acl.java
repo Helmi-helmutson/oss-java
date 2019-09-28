bf6e2e558fab0f8e23bb56db23ac39821527198d
Dipl. Ing. Péter Varkoly <peter@varkoly.de>
2019-09-28 14:21:08 +0200

    Do not remove group and user when deleting an acl.

--- oss-dao/src/main/java/de/openschoolserver/dao/Acl.java
+++ oss-dao/src/main/java/de/openschoolserver/dao/Acl.java
@@ -30,7 +30,7 @@ public class Acl implements Serializable {
 	private Boolean allowed;
 	
 	//bi-directional many-to-one association to User
-	@ManyToOne(cascade ={CascadeType.ALL})
+	@ManyToOne
 	@JsonIgnore
 	private User user;
 	
@@ -38,7 +38,7 @@ public class Acl implements Serializable {
 	private Long userId;
 
 	//bi-directional many-to-one association to Group
-	@ManyToOne(cascade ={CascadeType.ALL})
+	@ManyToOne
 	@JsonIgnore
 	private Group group;
 	
