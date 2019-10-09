9d75dec7ffa4ca66601eea6145537cf72463bbc3
Dipl. Ing. Péter Varkoly <peter@varkoly.de>
2019-10-09 22:30:06 +0200

    Add export HOME

--- oss-dao/data/updates/013-Add-Acl-system.addons.sh
+++ oss-dao/data/updates/013-Add-Acl-system.addons.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-
-SCHEDULER=$( echo "select id from Enumerates where name='apiAcl' AND value='system.addons';" | mysql OSS )
-if [ "$SCHEDULER" ]; then
-        echo "system.addons is already installed."
-        exit 0
+export HOME="/root/"
+SYSTEMADDON=$( echo "select id from Enumerates where name='apiAcl' AND value='system.addons';" | mysql OSS )
+if [ -z "$SYSTEMADDON" ]; then
+	echo "INSERT INTO Enumerates values(NULL,'apiAcl','system.addons',6);" | mysql OSS
 fi
-echo "INSERT INTO Enumerates values(NULL,'apiAcl','system.addons',6);" | mysql OSS
-echo "INSERT INTO Acls values(NULL,NULL,1,'system.addons','Y',6);"     | mysql OSS
+SYSTEMADDON=$( echo "select id from Acls where acl='system.addons' AND group_id=1;" | mysql OSS )
+if [ -z "$SYSTEMADDON" ]; then
+	echo "INSERT INTO Acls values(NULL,NULL,1,'system.addons','Y',6);"     | mysql OSS
+fi
 
 if [ -e /usr/lib/systemd/system/cephalix-api.service ]; then
         systemctl restart cephalix-api
